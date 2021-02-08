b8c35b4b8408ef7076bbaae4285cfe702f8c58c9
Philip Bradley <pbradley@fhcrc.org>
2021-02-08 14:20:26 -0800

    clean up the svg-to-png conversion script a bit, include new inkscape location and commandline flags

--- conga/convert_svg_to_png.py
+++ conga/convert_svg_to_png.py
@@ -2,15 +2,20 @@ from os import system
 from os.path import isfile
 from sys import stderr, exit
 
-# modify this if you have command line inkscape installed in a different place
+# modify this (ie PATH_TO_INKSCAPE) if you have command line inkscape installed
+#   in a different place
 # note that we also try just 'inkscape' first so if it's in your path you should be good.
 #
-PATH_TO_INKSCAPE='/Applications/Inkscape.app/Contents/Resources/bin/inkscape'
+PATH_TO_INKSCAPE='/Applications/Inkscape.app/Contents/MacOS/inkscape'
+ALT_PATH_TO_INKSCAPE='/Applications/Inkscape.app/Contents/Resources/bin/inkscape'
 
 # if the fonts in the tcr logos look bad, try uncommenting one of the other three lines below this one
 #MONOSPACE_FONT_FAMILY = 'monospace'
 #MONOSPACE_FONT_FAMILY = 'courier'
 MONOSPACE_FONT_FAMILY = 'DejaVu Sans Mono'
+
+if isfile(ALT_PATH_TO_INKSCAPE) and not isfile(PATH_TO_INKSCAPE):
+    PATH_TO_INKSCAPE = ALT_PATH_TO_INKSCAPE
 
 ## you could modify this function if you have a different cmdline tool for converting svg to png
 ## like cairosvg
@@ -23,6 +28,8 @@ def convert_svg_to_png(
         allow_missing=False,
         allow_failure=True
 ):
+    from shutil import which
+
     if not isfile(svgfile):
         errmsg = 'Error: convert_svg_to_png: svgfile does not exist: {}\n'.format(svgfile)
         print(errmsg)
@@ -31,28 +38,42 @@ def convert_svg_to_png(
             return
         else:
             exit()
-    cmd = 'convert {} {}'.format( svgfile, pngfile )
-    if verbose:
-        print(cmd)
-    system(cmd)
 
-    if isfile( pngfile ):
-        ## success
-        return
-    else:
-        print(f'conga.convert_svg_to_png: 1st try failed, command="{cmd}"')
+    if which('convert') is not None:
+        cmd = 'convert {} {}'.format( svgfile, pngfile )
+        if verbose:
+            print(cmd)
+        system(cmd)
 
-    ## cmdline inkscape
-    cmd = 'inkscape --export-png {} {}'.format( pngfile, svgfile )
-    if verbose:
-        print(cmd)
-    system(cmd)
+        if isfile( pngfile ):
+            ## success
+            return
+        else:
+            print(f'conga.convert_svg_to_png: convert failed, command="{cmd}"')
 
-    if isfile( pngfile ):
-        ## success
-        return
-    else:
-        print(f'conga.convert_svg_to_png: 2nd try failed, command="{cmd}"')
+    if which('inkscape') is not None:
+        ## cmdline inkscape: new options
+        cmd = 'inkscape -o {} {}'.format( pngfile, svgfile )
+        if verbose:
+            print(cmd)
+        system(cmd)
+
+        if isfile( pngfile ):
+            ## success
+            return
+        else:
+            print(f'conga.convert_svg_to_png: inkscape failed, command="{cmd}"')
+
+        cmd = 'inkscape --export-png {} {}'.format( pngfile, svgfile )
+        if verbose:
+            print(cmd)
+        system(cmd)
+
+        if isfile( pngfile ):
+            ## success
+            return
+        else:
+            print(f'conga.convert_svg_to_png: inkscape failed, command="{cmd}"')
 
     ## this is probably a long-shot, but in case inkscape is installed on mac
     if isfile( PATH_TO_INKSCAPE ):
@@ -60,6 +81,17 @@ def convert_svg_to_png(
         svgfile_full = abspath( svgfile )
         pngfile_full = abspath( pngfile )
 
+        # this is the new syntax
+        cmd = '{} -o {} {}'.format( PATH_TO_INKSCAPE, pngfile_full, svgfile_full )
+        if verbose:
+            print(cmd)
+        system(cmd)
+
+        if isfile( pngfile ):
+            ## success
+            return
+
+        # this is the old syntax
         cmd = '{} --export-png {} {}'.format( PATH_TO_INKSCAPE, pngfile_full, svgfile_full )
         if verbose:
             print(cmd)
@@ -72,29 +104,31 @@ def convert_svg_to_png(
             print(f'conga.convert_svg_to_png: inkscape failed, command="{cmd}"')
 
 
-    ## another possibility
-    cmd = 'rsvg-convert {} -o {}'.format( svgfile, pngfile )
-    if verbose:
-        print(cmd)
-    system(cmd)
+    if which('rsvg-convert') is not None:
+        ## another possibility
+        cmd = 'rsvg-convert {} -o {}'.format( svgfile, pngfile )
+        if verbose:
+            print(cmd)
+        system(cmd)
 
-    if isfile( pngfile ):
-        ## success
-        return
-    else:
-        print(f'conga.convert_svg_to_png: 3rd try failed, command="{cmd}"')
+        if isfile( pngfile ):
+            ## success
+            return
+        else:
+            print(f'conga.convert_svg_to_png: 3rd try failed, command="{cmd}"')
 
-    ## another possibility
-    cmd = 'magick convert {} {}'.format( svgfile, pngfile )
-    if verbose:
-        print(cmd)
-    system(cmd)
+    if which('magick') is not None:
+        ## another possibility
+        cmd = 'magick convert {} {}'.format( svgfile, pngfile )
+        if verbose:
+            print(cmd)
+        system(cmd)
 
-    if isfile( pngfile ):
-        ## success
-        return
-    else:
-        print(f'conga.convert_svg_to_png: 4th try failed, command="{cmd}"')
+        if isfile( pngfile ):
+            ## success
+            return
+        else:
+            print(f'conga.convert_svg_to_png: 4th try failed, command="{cmd}"')
 
 
     ## this might also occur if the svgfile were empty...
@@ -104,4 +138,3 @@ def convert_svg_to_png(
     if not allow_failure:
         exit()
     return
-
